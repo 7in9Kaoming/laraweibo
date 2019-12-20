8b49e12c3039163ec6aa9aaf09195355b6e56556
lingkaoming <lingkaoming@outlook.com>
2019-12-20 12:14:51 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,36 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Weibo App')</title>
-</head>
-<body>
-  @yield('content')
-</body>
+  <head>
+    <title>@yield('title', 'Weibo App')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+      <a class="navbar-brand" href="#">Weibo App</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav"> 
+	  <li class="nav-item">
+            <a class="nav-link" href="#">帮助</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">登录</a>
+          </li>
+	</ul>
+      </div>
+      <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>-->
+    </nav>    
+
+   <div class="container">
+     @yield('content')
+   </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+  </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.default')
 
-@section('title', 'Weibo APP - home')
-
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+  <h1 class="display-4">Hello, Laravel</h1>
+  <p class="lead">一切，从这里开始。</p>
+  <hr class="my-4">
+  <p class="lead">
+    <a class="btn btn-success btn-lg" href="#" role="button">现在注册</a>
+  </p>
+</div>
 @stop
