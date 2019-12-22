879c0a7b52824e979e3cd2a7f1e7a1491b004c0a
lingkaoming <lingkaoming@outlook.com>
2019-12-22 08:13:13 +0000

    中文错误信息提示

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,22 +8,25 @@
       <h5>注册</h5>
 	</div>
 	<div class="card-body">
+
+    @include('shared._errors')
+
 	<form action="{{ route('users.store') }}" method="POST">
 	  {{ csrf_field() }}
       <div class="form-group">
-        <label for="exampleInputEmail1">昵称：</label>
+        <label for="name">昵称：</label>
         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
       </div>
       <div class="form-group">
-        <label for="exampleInputEmail1">邮箱：</label>
-        <input type="email" name="email" class="form-control" value="{{ old('name') }}">
+        <label for="email">邮箱：</label>
+        <input type="email" name="email" class="form-control" value="{{ old('email') }}">
       </div>
       <div class="form-group">
-        <label for="exampleInputEmail1">密码：</label>
+        <label for="password">密码：</label>
         <input type="password" name="password"  class="form-control" value="{{ old('password') }}">
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">确认密码：</label>
+        <label for="password_confirmation">确认密码：</label>
         <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
       </div>
       <button type="submit" class="btn btn-primary">注册</button>
