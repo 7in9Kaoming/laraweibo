48363648206943b55d254eb4e0196acedb7a2417
lingkaoming <lingkaoming@outlook.com>
2019-12-22 04:18:32 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://laravel.com/img/logomark.min.svg">
+  刻意练习，每日精进
+
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,32 +5,14 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-      <a class="navbar-brand" href="#">Weibo App</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    @include('layouts._header')
+   
+    <div class="container">
+      @yield('content')
 
-      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-        <ul class="navbar-nav"> 
-	  <li class="nav-item">
-            <a class="nav-link" href="#">帮助</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">登录</a>
-          </li>
-	</ul>
-      </div>
-      <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>-->
-    </nav>    
-
-   <div class="container">
-     @yield('content')
-   </div>
-
+      @include('layouts._footer')
+    </div>
+ 
     <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
