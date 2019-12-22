334311b3a576db6ce8c20f951ece12bb84e39217
lingkaoming <lingkaoming@outlook.com>
2019-12-22 12:44:32 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    /**
+    * 显示登录表单
+    */
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    /**
+    * 登录逻辑处理
+    */
+    public function store(Request $request)
+    {
+        // 获取并验证登录凭证
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        }
+        session()->flash('danger', '佷抱歉，您的邮箱和密码不匹配');
+        return redirect()->back()->withInput();
+    }
+}
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -6,18 +6,18 @@
       </button>
 
       <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-        <ul class="navbar-nav"> 
+        <ul class="navbar-nav">
 	  <li class="nav-item">
             <a class="nav-link" href="{{ route('help') }}">帮助</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">登录</a>
+            <a class="nav-link" href="{{ route('login') }}">登录</a>
           </li>
 	</ul>
       </div>
       <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>-->
-    </nav>    
+    </nav>
 
 
