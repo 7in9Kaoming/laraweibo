0a36ce94737d1d3cc6872557b51ba472e3eae184
lingkaoming <lingkaoming@outlook.com>
2019-12-22 04:40:33 +0000

    命名路由

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="jumbotron">
-  <h1 class="display-4">Hello, Laravel</h1>
-  <p class="lead">一切，从这里开始。</p>
-  <hr class="my-4">
-  <p class="lead">
-    <a class="btn btn-success btn-lg" href="#" role="button">现在注册</a>
-  </p>
-</div>
+    <div class="jumbotron">
+      <h1 class="display-4">Hello, Laravel</h1>
+      <p class="lead">一切，从这里开始。</p>
+      <hr class="my-4">
+      <p class="lead">
+        <a class="btn btn-success btn-lg" href="#" role="button">现在注册</a>
+      </p>
+    </div>
 @stop
