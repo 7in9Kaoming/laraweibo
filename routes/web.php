77fd520278b1a512bba2c6d703c3dcebc19ffbfe
lingkaoming <lingkaoming@outlook.com>
2019-12-25 07:27:56 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,23 @@
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('help', 'StaticPagesController@help')->name('help');
+Route::get('about', 'StaticPagesController@about')->name('about');
 
-Route::get('/signup', 'UsersController@create')->name('signup');
-Route::resource('users', 'UsersController');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
-Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+Route::resource('users', 'UsersController');
+
+// 重置密码邮箱发送页面
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+// 邮箱发送密码重置链接
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+// 密码更新页面
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+// 密码更新操作
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
